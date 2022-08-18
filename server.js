67246e357951dcb5b006f2729b701e407bcd1d62
Nikita Smagin <nikita.s@prmnd.com>
2022-08-18 12:50:17 +0500

    fix cors

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: false,
+        origin: 'http://localhost:8080',
     }
 });
 const swaggerUi = require('swagger-ui-express');
